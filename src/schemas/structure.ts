cd0103a1f518161249c590e08b96563a53cc38fc
Matt Hillsdon <matt.hillsdon@microbit.org>
2024-03-26 10:03:12 +0000

    Top of structure builder should be a list.

--- src/schemas/structure.ts
+++ src/schemas/structure.ts
@@ -4,36 +4,31 @@
  * SPDX-License-Identifier: MIT
  */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import {FaCog as ConfigIcon, FaPython} from 'react-icons/fa'
+import {FaCog as ConfigIcon} from 'react-icons/fa'
 import {StructureBuilder} from 'sanity/structure'
 
 import {singletonPage, sourceDocumentTypeList} from '../common/structureUtils'
 
 const structure = (S: StructureBuilder) =>
-  S.listItem()
-    .title('Python editor')
-    .icon(FaPython)
-    .child(
-      S.list()
-        .title('Python editor')
-        .items([
-          S.listItem()
-            .title('Ideas')
-            .child(
-              S.list()
-                .id('python-ideas')
-                .items([
-                  S.listItem().title('Ideas').child(sourceDocumentTypeList(S, 'pythonIdea')),
-                  singletonPage(S, 'pythonIdeasConfig', 'Config').icon(ConfigIcon),
-                ]),
-            ),
-          S.listItem().title('Toolkits').child(sourceDocumentTypeList(S, 'toolkit')),
-          S.listItem().title('Toolkit topics').child(sourceDocumentTypeList(S, 'toolkitTopic')),
-          S.listItem()
-            .title('Toolkit topic entries')
-            .child(sourceDocumentTypeList(S, 'toolkitTopicEntry')),
-          S.listItem().title('Python modules').child(sourceDocumentTypeList(S, 'pythonModule')),
-        ]),
-    )
+  S.list()
+    .title('Python Editor')
+    .items([
+      S.listItem()
+        .title('Ideas')
+        .child(
+          S.list()
+            .id('python-ideas')
+            .items([
+              S.listItem().title('Ideas').child(sourceDocumentTypeList(S, 'pythonIdea')),
+              singletonPage(S, 'pythonIdeasConfig', 'Config').icon(ConfigIcon),
+            ]),
+        ),
+      S.listItem().title('Toolkits').child(sourceDocumentTypeList(S, 'toolkit')),
+      S.listItem().title('Toolkit topics').child(sourceDocumentTypeList(S, 'toolkitTopic')),
+      S.listItem()
+        .title('Toolkit topic entries')
+        .child(sourceDocumentTypeList(S, 'toolkitTopicEntry')),
+      S.listItem().title('Python modules').child(sourceDocumentTypeList(S, 'pythonModule')),
+    ])
 
 export default structure
